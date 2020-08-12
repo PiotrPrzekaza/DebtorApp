2f12f1fdd8f163c58e882fe9100c3a2701fbfdea
Piotr Przekaza <piotr.przekaza@hotmail.com>
2020-08-12 17:33:25 +0200

    added in debtorcore logic to create delete and show of borrowers

---
+++ Debtor.Core/BorrowerManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace Debtor.Core
+{
+    public class BorrowerManager
+    {
+        private List<Borrower> Borrowers { get; set; }
+
+        public void AddBorrower(string name, decimal amount)
+        {
+            var borrower = new Borrower
+            {
+                Name = name,
+                CashAmount = amount
+            };
+
+            Borrowers.Add(borrower);
+        }
+
+        public void DeleteBorrower(string name)
+        {
+            foreach(var borrower in Borrowers)
+            {
+                if (borrower.Name == name)
+                {
+                    Borrowers.Remove(borrower);
+                    return;
+                }
+            }
+        }
+        public List<string> ListBorrowers()
+        {
+            var borrowersListOfStrings = new List<string>();
+            var indexOfBorrowersList = 1;
+
+            foreach( var borrower in Borrowers)
+            {
+                var borrowerString = indexOfBorrowersList + ". " + borrower.Name + " - " + borrower.CashAmount + " zł !";
+                indexOfBorrowersList++;
+                borrowersListOfStrings.Add(borrowerString);
+            }
+
+
+            return borrowersListOfStrings;
+        }
+    }
+}
