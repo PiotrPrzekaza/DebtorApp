e8e44393ecbb51d8e1bae211904994b6344f6a64
Piotr Przekaza <piotr.przekaza@hotmail.com>
2020-08-12 19:54:17 +0200

    added save you debtor to file and read  from file

--- Debtor.Core/Borrower.cs
+++ Debtor.Core/Borrower.cs
@@ -4,5 +4,9 @@
     {
         public string Name { get; set; }
         public decimal CashAmount { get; set; }
+        public override string ToString()
+        {
+            return Name + ";" + CashAmount.ToString();
+        }
     }
 }
--- Debtor/DebtorApp.cs
+++ Debtor/DebtorApp.cs
@@ -77,7 +77,6 @@ namespace Debtor
                         break;
 
                 }
-                return;
             }
             
         }
