d44ee487a9bfe0e4f34e2a8846e1488e1e195da0
Piotr Przekaza <piotr.przekaza@hotmail.com>
2020-08-12 16:55:43 +0200

    added list of functionality will have a inmplement in feuture

---
+++ Debtor/DebtorApp.cs
@@ -0,0 +1,22 @@
+﻿namespace Debtor
+{
+    public class DebtorApp
+    {
+        public void IntroductionDebtor()
+        {
+
+        }
+        public void AddBorrower()
+        {
+
+        }
+        public void DeleteBorrower()
+        {
+
+        }
+        public void ListAllBorrower()
+        {
+
+        }
+    }
+}
